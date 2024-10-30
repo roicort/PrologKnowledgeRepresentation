1edb731cfc2694e5391399268a87a685a3550e74
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2024-10-30 15:38:54 -0600

    Checkpoint

--- KnowledgeBase.pl
+++ KnowledgeBase.pl
@@ -120,58 +120,271 @@ classes_of_individual(Individual, KnowledgeBase, Classes) :-
     findall(ParentClass, (member(Class, DirectClasses), parentclasses_of(Class, KnowledgeBase, ParentClass)), ParentClasses),
     flatten([DirectClasses | ParentClasses], Classes).
 
-%% 2. Predicados para añadir
+%% 2. Predicados para añadir 
 
-% A. Añadir una clase
+%A. Clases u objetos cuyo nombre será add_class y add_object, respectivamente. 
+%Ambos predicados recibirán cuatro argumentos: (i) el nombre de la clase u objeto a añadir, (ii) el 
+%nombre de la clase madre, (iii) la base de conocimientos actual, y (iv) la nueva base de 
+%conocimientos donde se refleja la adición.
+
+% A1. Añadir una clase
 
 add_class(NewClass,Father,KnowledgeBase,NewKnowledgeBase) :-
     member(class(Father,_,_,_,_),KnowledgeBase),
     not(member(class(NewClass,_,_,_,_),KnowledgeBase)),
     append(KnowledgeBase,[class(NewClass,Father,[],[],[])],NewKnowledgeBase).
 
-% B. Añadir un objeto
+% A2. Añadir un objeto
 
 add_object(Object, Class, KnowledgeBase, NewKnowledgeBase) :-
     member(class(Class, _, _, _, Objects), KnowledgeBase),
     not(member([id=>Object, _, _], Objects)),
     select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, Relations, [[id=>Object, [], []] | Objects]), NewKnowledgeBase).
 
+%B. Propiedades de clases u objetos cuyo nombre será add_class_property y 
+%add_object_property
+
+%Ambos predicados recibirán cinco argumentos: (i) 
+%el nombre de la clase u objeto, (ii) el nombre de la propiedad a añadir, (iii) el valor de 
+%dicha propiedad, (iv) la base de conocimientos actual, y (v) la nueva base de 
+%conocimientos donde se refleja la adición. 
+
+% B1. Añadir una propiedad a una clase add_object_property
+
+add_class_property(Class, Property, Value, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
+    not(member(Property=>Value, Properties)),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, [[Property=>Value] | Properties], Relations, Objects), NewKnowledgeBase).
+
+% B2. Añadir una propiedad a un objeto add_object_property
+
+add_object_property(Object, Property, Value, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, _, _, _, Objects), KnowledgeBase),
+    member([id=>Object, Properties, Relations], Objects),
+    not(member(Property=>Value, Properties)),
+    select([id=>Object, Properties, Relations], Objects, [id=>Object, [[Property=>Value] | Properties], Relations], NewObjects),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, Relations, NewObjects), NewKnowledgeBase).
+
+% C. Relaciones de clases u objetos cuyo nombre será add_class_relation y add_object_relation.
+
+%Ambos predicados recibirán cinco argumentos: (i) el nombre de la clase 
+%u objeto, (ii) el nombre de la relación a añadir, (iii) la o las clases con quienes se guarda la 
+%relación, si se trata de add_class_relation, y el o los objetos con quienes se guarda la 
+%relación, si el predicado es add_object_relation , (iv) la base de conocimientos actual, y (v) 
+%la nueva base de conocimientos donde se refleja la adición. 
+
+% C1. Añadir una relación a una clase
+
+add_class_relation(Class, Relation, Target, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
+    not(member(Relation=>Target, Relations)),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, [[Relation=>Target] | Relations], Objects), NewKnowledgeBase).
+
+% C2. Añadir una relación a un objeto
+
+add_object_relation(Object, Relation, Target, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
+    member([id=>Object, ObjProperties, ObjRelations], Objects),
+    not(member(Relation=>Target, ObjRelations)),
+    select([id=>Object, ObjProperties, ObjRelations], Objects, [id=>Object, ObjProperties, [Relation=>Target | ObjRelations]], NewObjects),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, Relations, NewObjects), NewKnowledgeBase).
+
 %% 3. Predicados para eliminar
 
-% A. Eliminar una clase
+%A. Clases u objetos cuyo nombre será rm_class y rm_object, respectivamente. 
+
+%Ambos predicados recibirán tres argumentos: (i) el nombre de la clase u objeto a eliminar, (ii) la 
+%base de conocimientos actual, y (iii) la nueva base de conocimientos donde se refleja la 
+%eliminación.
+
+% A1. Eliminar una clase
 
 delete_class(Class, KnowledgeBase, NewKnowledgeBase) :-
     exclude([class(Class, _, _, _, _)]>>true, KnowledgeBase, NewKnowledgeBase).
 
-% B. Eliminar un objeto
+% A2. Eliminar un objeto
 
 delete_object(Object, Class, KnowledgeBase, NewKnowledgeBase) :-
     member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
     select([id=>Object, _, _], Objects, NewObjects),
     select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, Relations, NewObjects), NewKnowledgeBase).
 
+% B. Propiedades específicas de clases u objetos cuyo nombre será rm_class_property y rm_object_property. 
+
+%Ambos predicados recibirán cuatro argumentos: (i) 
+%el nombre de la clase u objeto, (ii) el nombre de la propiedad a eliminar, (iii) la base de 
+%conocimientos actual, y (iv) la nueva base de conocimientos donde se refleja la 
+%eliminación.
+
+% B1. Eliminar una propiedad de una clase
+
+delete_class_property(Class, Property, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
+    select([Property=>_], Properties, NewProperties),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, NewProperties, Relations, Objects), NewKnowledgeBase).
+
+% B2. Eliminar una propiedad de un objeto
+
+delete_object_property(Object, Class, Property, KnowledgeBase, NewKnowledgeBase) :-
+    % Encontrar la clase que contiene el objeto
+    member(class(Class, Father, ClassProperties, ClassRelations, Objects), KnowledgeBase),
+    % Encontrar el objeto dentro de la lista de objetos de la clase
+    member([id=>Object, ObjProperties, ObjRelations], Objects),
+    % Eliminar la propiedad del objeto
+    select(Property=>_, ObjProperties, UpdatedProperties),
+    % Actualizar la lista de objetos con las nuevas propiedades del objeto
+    select([id=>Object, ObjProperties, ObjRelations], Objects, [id=>Object, UpdatedProperties, ObjRelations], NewObjects),
+    % Actualizar la clase con la nueva lista de objetos
+    select(class(Class, Father, ClassProperties, ClassRelations, Objects), KnowledgeBase, class(Class, Father, ClassProperties, ClassRelations, NewObjects), NewKnowledgeBase).
+
+% C. Relaciones específicas de clases u objetos cuyo nombre será rm_class_relation y rm_object_relation. 
+
+%Ambos predicados recibirán cuatro argumentos: (i) 
+%el nombre de la clase u objeto, (ii) el nombre de la relación a eliminar, (iii) la base de 
+%conocimientos actual, y (iv) la nueva base de conocimientos donde se refleja la eliminación. 
+
+% C1. Eliminar una relación de una clase
+
+delete_class_relation(Class, Relation, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
+    select([Relation=>_], Relations, NewRelations),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, NewRelations, Objects), NewKnowledgeBase).
+
+% C2. Eliminar una relación de un objeto
+
+delete_object_relation(Object, Class, Relation, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, _, _, _, Objects), KnowledgeBase),
+    member([id=>Object, Properties, Relations], Objects),
+    select([Relation=>_], Relations, NewRelations),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, NewRelations, Objects), NewKnowledgeBase).
+
+%% 4. Predicados para modificar
+
+%A. Cambiar el nombre de una clase u objeto cuyo nombre será change_class_name y change_object_name, respectivamente.
+
+%Ambos predicados recibirán cuatro argumentos: (i) 
+%el nombre de la clase u objeto a modificar, (ii) el nuevo nombre para dicha clase u objeto, 
+%(iii) la base de conocimientos actual, y (iv) la nueva base de conocimientos donde se 
+%refleja la modificación.
+
+% A1. Cambiar el nombre de una clase
+
+change_class_name(OldName, NewName, KnowledgeBase, NewKnowledgeBase) :-
+    select(class(OldName, Father, Properties, Relations, Objects), KnowledgeBase, class(NewName, Father, Properties, Relations, Objects), NewKnowledgeBase).
+
+% A2. Cambiar el nombre de un objeto
+
+change_object_name(OldName, NewName, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(ClassName, ParentClass, Properties, Relations, Objects), KnowledgeBase),
+    select([id=>OldName | RestProps], Objects, RestObjects),
+    select(class(ClassName, ParentClass, Properties, Relations, Objects), KnowledgeBase, class(ClassName, ParentClass, Properties, Relations, [[id=>NewName | RestProps] | RestObjects]), NewKnowledgeBase).
+
+% B. El valor de una propiedad específica de una clase u objeto cuyo nombre será change_value_class_property y change_value_object_property, respectivamente.
+
+%Ambos predicados recibirán cinco argumentos: (i) el nombre de la clase u objeto, (ii) el nombre 
+%de la propiedad a modificar, (iii) el nuevo valor de dicha propiedad, (iv) la base de 
+%conocimientos actual, y (v) la nueva base de conocimientos donde se refleja la 
+%modificación.
+
+% B1. Cambiar el valor de una propiedad de una clase
+
+change_value_class_property(Class, Property, NewValue, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
+    select([Property=>_], Properties, NewProperties),
+    append(NewProperties, [[Property=>NewValue]], UpdatedProperties),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, UpdatedProperties, Relations, Objects), NewKnowledgeBase).
+
+% B2. Cambiar el valor de una propiedad de un objeto
+
+change_value_object_property(Object, Property, NewValue, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(ClassName, ParentClass, Properties, Relations, Objects), KnowledgeBase),
+    member([id=>Object, ObjProperties, ObjRelations], Objects),
+    select([Property=>_], ObjProperties, NewProperties),
+    append(NewProperties, [[Property=>NewValue]], UpdatedProperties),
+    select([id=>Object, ObjProperties, ObjRelations], Objects, [id=>Object, UpdatedProperties, ObjRelations], NewObjects),
+    select(class(ClassName, ParentClass, Properties, Relations, Objects), KnowledgeBase, class(ClassName, ParentClass, Properties, Relations, NewObjects), NewKnowledgeBase).
+    
+% C. Con quién mantiene una relación específica una clase u objeto cuyo nombre será change_value_class_relation y change_value_object_relation, respectivamente. 
+
+%Ambos predicados recibirán cinco argumentos: (i) el nombre de la clase u objeto, (ii) el nombre 
+%de la relación a modificar, (iii) la o las clases con quienes ahora se guarda la relación, si se 
+%trata de change_value_class_relation, y el o los objetos con quienes ahora se guarda la 
+%relación, si el predicado es change_value_object_relation, (iv) la base de conocimientos 
+%actual, y (v) la nueva base de conocimientos donde se refleja la modificación.
+
+% C1. Cambiar el valor de una relación de una clase
+
+change_value_class_relation(Class, Relation, NewTarget, KnowledgeBase, NewKnowledgeBase) :-
+    member(class(Class, Father, Properties, Relations, Objects), KnowledgeBase),
+    select([Relation=>_], Relations, NewRelations),
+    append(NewRelations, [[Relation=>NewTarget]], UpdatedRelations),
+    select(class(Class, Father, Properties, Relations, Objects), KnowledgeBase, class(Class, Father, Properties, UpdatedRelations, Objects), NewKnowledgeBase).
+
+% C2. Cambiar el valor de una relación de un objeto
+
+change_value_object_relation(Object, Relation, NewTarget, KnowledgeBase, NewKnowledgeBase) :-
+    % Encontrar la clase que contiene el objeto
+    member(class(ClassName, ParentClass, Properties, Relations, Objects), KnowledgeBase),
+    % Encontrar el objeto dentro de la lista de objetos de la clase
+    member([id=>Object, ObjProperties, ObjRelations], Objects),
+    % Cambiar el valor de la relación
+    select(Relation=>_, ObjRelations, TempRelations),
+    append([Relation=>NewTarget], TempRelations, UpdatedRelations),
+    % Actualizar la lista de objetos con las nuevas relaciones del objeto
+    select([id=>Object, ObjProperties, ObjRelations], Objects, [id=>Object, ObjProperties, UpdatedRelations], NewObjects),
+    % Actualizar la clase con la nueva lista de objetos
+    select(class(ClassName, ParentClass, Properties, Relations, Objects), KnowledgeBase, class(ClassName, ParentClass, Properties, Relations, NewObjects), NewKnowledgeBase).
+
 %% Main
 
 main :-
 
+    %% Inicialización %%
+
     % Cargar modulo I/O
     consult('IO.pl'),
-    % Cargar base de conocimiento
+    % Cargar base de conocimieento
     open_kb('KB.txt', KnowledgeBase1),
+
+    %%% Pruebas añadir %%%
+
+    write('Añadiendo...'), nl,
+
+    add_class(paphiopedilum, orchidaceae, KnowledgeBase1, Update1),
+    add_object(rosy, rosas, Update1, Update2),
+
+    add_class_property(paphiopedilum, altura, '30cm', Update2, Update3),
+    add_object_property(rosy, color, rojo, Update3, Update4),
+
+    add_class_relation(ornitorrincos, comen, paphiopedilum, Update4, Update5),
+    add_object_relation(perry, comen, rosy, Update5, Update6),
+
+    %%% Pruebas modificar %%%
+
+    write('Modificando...'), nl,
+
+    change_class_name(paphiopedilum, paphiopedilumdelenatii, Update6, Update7),
+    change_object_name(rosy, rositafresita, Update7, Update8),
+
+    change_value_class_property(paphiopedilumdelenatii, altura, '22cm', Update8, Update9),
+    change_value_object_property(rositafresita, color, rosa, Update9, Update10),
+
+    change_value_class_relation(ornitorrincos, comen, paphiopedilumdelenatii, Update10, Update11),
+    change_value_object_relation(perry, comen, rositafresita, Update11, Update12),
+
+    %% Guardar base de conocimiento %%
+
+    save_kb('KB.txt', Update12),
+    open_kb('KB.txt', KnowledgeBase2),
+
+    %% Pruebas de predicados %%
+
+    write('Consultando...'), nl,
 
     subclasses_of(animales, KnowledgeBase1, AnimalSubclasses),
     format('Subclases de animales: ~w~n', [AnimalSubclasses]),
     subclasses_of(plantas, KnowledgeBase1, PlantasSubclasses),
     format('Subclases de plantas: ~w~n', [PlantasSubclasses]),
-
-    add_class(paphiopedilum, orchidaceae, KnowledgeBase1, Update1),
-    add_object(rositafresita, rosas, Update1, Update2),
-    save_kb('KB.txt', Update2),
-    format('Clase paphiopedilum añadida~n'),
-
-
-
-    open_kb('KB.txt', KnowledgeBase2),
 
     subclasses_of(orchidaceae, KnowledgeBase2, OrchidaceaeSubclasses),
     format('Subclases de orchidaceae: ~w~n', [OrchidaceaeSubclasses]),
@@ -201,8 +414,21 @@ main :-
     classes_of_individual(rositafresita, KnowledgeBase2, RositaClasses),
     format('Clases de Rosita Fresita: ~w~n', [RositaClasses]),
 
-    delete_class(paphiopedilum, KnowledgeBase2, Update3),
-    delete_object(rositafresita, rosas, Update3, Update4),
-    save_kb('KB.txt', Update4),
+    %% Pruebas eliminar %%
+
+    write('Eliminando...'), nl,
+
+    delete_class_property(paphiopedilumdelenatii, altura, Update12, Update13),
+    delete_object_property(rositafresita, rosas, color, Update13, Update14),
+
+    %delete_class_relation(ornitorrincos, comen, Update14, Update15),
+    %delete_object_relation(perry, rositafresita, comen, Update15, Update16),
+
+    %delete_class(paphiopedilumdelenatii, Update16, Update17),
+    %delete_object(rositafresita, rosas, Update17, Update18),
+
+    save_kb('KB.txt', Update14),
+
+    %% Fin %%
 
     halt.
